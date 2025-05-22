f1dc2632ba8aacf755a49636b9181c3511461e54
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-05-22 13:16:00 +0900

    first commit

---
+++ src/main/java/Portfolio/BlueprintsManagement/BlueprintsManagementApplication.java
@@ -0,0 +1,13 @@
+package Portfolio.BlueprintsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueprintsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueprintsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Portfolio/BlueprintsManagement/BlueprintsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Portfolio.BlueprintsManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
