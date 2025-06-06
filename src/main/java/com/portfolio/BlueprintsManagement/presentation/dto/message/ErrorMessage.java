61b130432dd52b247e89acf78c48ec877b948c78
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-06 16:18:23 +0900

    メッセージの変更

--- src/main/java/com/portfolio/BlueprintsManagement/presentation/dto/message/ErrorMessage.java
+++ src/main/java/com/portfolio/BlueprintsManagement/presentation/dto/message/ErrorMessage.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 public enum ErrorMessage {
     NOT_FOUND_SITES("現場が追加されていません"),
     NOT_FOUND_SITE_BY_ID("idに該当する現場が存在しません"),
+    NOT_FOUND_BLUEPRINT_BY_SITE_ID("現場idに該当する図面情報が存在しません"),
+    NOT_FOUND_BLUEPRINT_BY_ID("idに該当する図面情報が存在しません"),
     ID_MUST_BE_UUID("idの形式が正しくありません"),
     CHAR_COUNT_SITE_NAME_TOO_LONG("現場名は50文字以内で入力してください"),
     CHAR_COUNT_ADDRESS_TOO_LONG("住所は161文字以内で入力してください"),
--- src/main/java/com/portfolio/BlueprintsManagement/presentation/dto/message/SuccessMessage.java
+++ src/main/java/com/portfolio/BlueprintsManagement/presentation/dto/message/SuccessMessage.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessMessage {
-    COMPLETE_REGISTER_SITE("新規現場が登録されました"),
     COMPLETE_UPDATE_SITE("現場情報が更新されました"),
     COMPLETE_DELETE_SITE("現場が削除されました");
 
