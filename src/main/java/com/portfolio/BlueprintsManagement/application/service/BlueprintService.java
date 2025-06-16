4ca8f5369f383b03d33e8ab292f35e7e06b70fce
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-16 12:49:36 +0900

    ファイルアップロードをs3に変更

--- src/main/java/com/portfolio/BlueprintsManagement/application/service/BlueprintService.java
+++ src/main/java/com/portfolio/BlueprintsManagement/application/service/BlueprintService.java
@@ -11,17 +11,21 @@ import com.portfolio.BlueprintsManagement.presentation.dto.request.blueprint.Del
 import com.portfolio.BlueprintsManagement.presentation.dto.request.blueprint.UpdateBlueprintRequest;
 import com.portfolio.BlueprintsManagement.presentation.exception.customException.NotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +33,20 @@ public class BlueprintService {
 
     private final IBlueprintRepository blueprintRepository;
     private final IArchitecturalDrawingRepository architecturalDrawingRepository;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+    AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
+
+    private String bucketName = "blueprints-management-bucket";
+    Region region = Region.AP_NORTHEAST_1;
+    S3Client s3 = S3Client.builder().region(region).credentialsProvider(credentialsProvider).build();
+
 
     public List<Blueprint> getBlueprintsBySiteId(String siteId) throws NotFoundException {
         if (!blueprintRepository.existBlueprintBySiteId(siteId)) {
@@ -50,18 +68,26 @@ public class BlueprintService {
     public String addBlueprint(AddBlueprintRequest request) throws IOException {
         MultipartFile imageFile = request.getImageFile();
         String imageFileName = imageFile.getOriginalFilename();
-        Resource resource = new ClassPathResource("static/image");
-        Path imageDir = resource.getFile().toPath();
+//        Resource resource = new ClassPathResource("static/image");
+//        Path imageDir = resource.getFile().toPath();
         byte[] content = imageFile.getBytes();
-        Path filePath = imageDir.resolve(Objects.requireNonNull(imageFileName));
+//        Path filePath = imageDir.resolve(Objects.requireNonNull(imageFileName));
 
         Blueprint blueprint = Blueprint.formBlueprint(request);
-        ArchitecturalDrawing architecturalDrawing = ArchitecturalDrawing.formArchitecturalDrawingFromBlueprintRequest(request, blueprint.getId(), "image/" + imageFileName);
+        String filePath = blueprint.getSiteId() + "/" + blueprint.getId() + "/" + imageFileName;
+
+        ArchitecturalDrawing architecturalDrawing = ArchitecturalDrawing.formArchitecturalDrawingFromBlueprintRequest(request, blueprint.getId(), filePath);
 
         blueprintRepository.addBlueprint(blueprint);
         architecturalDrawingRepository.addArchitecturalDrawing(architecturalDrawing);
 
-        Files.write(filePath, content);
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName).key(filePath).build();
+
+        s3.putObject(objectRequest, RequestBody.fromBytes(content));
+
+//      Files.write(filePath, content);
         return blueprint.getId();
     }
 
