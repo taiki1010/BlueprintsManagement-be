4fb5ef2143af1b148111d0fa1ce7f3a254f84970
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-17 16:04:57 +0900

    ArchitecturalDrawingRequest.javaでsiteIdが受け取れるように修正

--- src/main/java/com/portfolio/BlueprintsManagement/presentation/dto/request/architecturalDrawing/ArchitecturalDrawingRequest.java
+++ src/main/java/com/portfolio/BlueprintsManagement/presentation/dto/request/architecturalDrawing/ArchitecturalDrawingRequest.java
@@ -12,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class ArchitecturalDrawingRequest {
 
     @ValidId
+    private String siteId;
+
+    @ValidId
     private String BlueprintId;
 
     @ValidCreatedAt
