3fb70c098853aab85ae94cda4d3370e6f1795989
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-18 13:42:12 +0900

    テストしやすいようにserviceの記述を変更

--- src/main/java/com/portfolio/BlueprintsManagement/application/service/ArchitecturalDrawingService.java
+++ src/main/java/com/portfolio/BlueprintsManagement/application/service/ArchitecturalDrawingService.java
@@ -3,38 +3,42 @@ package com.portfolio.BlueprintsManagement.application.service;
 import com.portfolio.BlueprintsManagement.domain.model.architecturalDrawing.ArchitecturalDrawing;
 import com.portfolio.BlueprintsManagement.domain.repository.IArchitecturalDrawingRepository;
 import com.portfolio.BlueprintsManagement.presentation.dto.request.architecturalDrawing.ArchitecturalDrawingRequest;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 
 @Service
-@RequiredArgsConstructor
 public class ArchitecturalDrawingService {
 
     private final IArchitecturalDrawingRepository architecturalDrawingRepository;
 
-    @Value("${s3.bucket.name}")
-    private String bucketName;
-    Region region = Region.AP_NORTHEAST_1;
-    S3Client s3 = S3Client.builder().region(region).build();
+    private final S3Client s3;
+    private final String bucketName;
+
+    @Autowired
+    public ArchitecturalDrawingService(
+            IArchitecturalDrawingRepository architecturalDrawingRepository,
+            S3Client s3,
+            @Value("${s3.bucket.name}") String bucketName
+    ) {
+        this.architecturalDrawingRepository = architecturalDrawingRepository;
+        this.s3 = s3;
+        this.bucketName = bucketName;
+    }
 
     @Transactional
     public ArchitecturalDrawing addArchitecturalDrawing(ArchitecturalDrawingRequest request) throws IOException {
 
         MultipartFile imageFile = request.getImageFile();
         String imageFileName = imageFile.getOriginalFilename();
-//        Resource resource = new ClassPathResource("static/image");
-//        Path imageDir = resource.getFile().toPath();
         byte[] content = imageFile.getBytes();
-//        Path filePath = imageDir.resolve(Objects.requireNonNull(imageFileName));
         String filePath = request.getBlueprintId() + "/" + imageFileName;
 
         ArchitecturalDrawing architecturalDrawing = ArchitecturalDrawing.formArchitecturalDrawing(request, filePath);
@@ -49,8 +53,6 @@ public class ArchitecturalDrawingService {
         } catch (Exception e) {
             throw new RuntimeException("ファイルのS3へのアップロードに失敗しました", e);
         }
-
-//        Files.write(filePath, content);
 
         return architecturalDrawing;
 
