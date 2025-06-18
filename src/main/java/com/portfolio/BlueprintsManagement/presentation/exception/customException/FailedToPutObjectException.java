d59eb2cc50d4326c583e942af8906ad37ed090a3
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-18 15:43:49 +0900

    カスタム例外の追加

--- src/main/java/com/portfolio/BlueprintsManagement/presentation/exception/customException/FailedToPutObjectException.java
+++ src/main/java/com/portfolio/BlueprintsManagement/presentation/exception/customException/FailedToPutObjectException.java
@@ -1,7 +1,11 @@
 package com.portfolio.BlueprintsManagement.presentation.exception.customException;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class FailedToPutObjectException extends RuntimeException {
-  public FailedToPutObjectException(String message) {
-    super(message);
-  }
+
+    private String message;
 }
