eb28d3ffc6cbeaa92d05d15d45d4beb7c098c90d
taiki1010 <tik86_trueno@yahoo.co.jp>
2025-06-24 14:01:54 +0900

    モデルにOpenAPIドキュメントを記述

--- src/main/java/com/portfolio/BlueprintsManagement/domain/model/site/Site.java
+++ src/main/java/com/portfolio/BlueprintsManagement/domain/model/site/Site.java
@@ -1,20 +1,29 @@
 package com.portfolio.BlueprintsManagement.domain.model.site;
 
 import com.portfolio.BlueprintsManagement.presentation.dto.request.site.SiteRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Schema(description = "現場")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Site {
 
+    @Schema(description = "現場ID", example = "00000000-0000-1000-8000-000000000001")
     private String id;
+
+    @Schema(description = "現場名", example = "佐藤邸")
     private String name;
+
+    @Schema(description = "住所", example = "東京都表参道")
     private String address;
+
+    @Schema(description = "備考", example = "コンビニの近く")
     private String remark;
 
     /**
